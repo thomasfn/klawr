8f33dd2be3fb26605852a440cb0a840dcc1b7ed3
Robert Acksel <robert.acksel@googlemail.com>
2016-09-29 20:33:35 +0200

    -readd acedently delete

--- Klawr/Source/KlawrCodeGeneratorPlugin/KlawrCodeGeneratorPlugin.Build.cs
+++ Klawr/Source/KlawrCodeGeneratorPlugin/KlawrCodeGeneratorPlugin.Build.cs
@@ -3,59 +3,59 @@ using UnrealBuildTool;
 
 namespace UnrealBuildTool.Rules
 {
-	public class KlawrCodeGeneratorPlugin : ModuleRules
-	{
-		public KlawrCodeGeneratorPlugin(TargetInfo Target)
-		{
+    public class KlawrCodeGeneratorPlugin : ModuleRules
+    {
+        public KlawrCodeGeneratorPlugin(TargetInfo Target)
+        {
             PublicIncludePaths.AddRange(
-				new string[] {
-					"Programs/UnrealHeaderTool/Public",
+                new string[] {
+                    "Programs/UnrealHeaderTool/Public",
                     "KlawrCodeGeneratorPlugin/Public",
-					// ... add other public include paths required here ...
-				}
-			);
+                    // ... add other public include paths required here ...
+                }
+            );
 
-			PrivateIncludePaths.AddRange(
-				new string[] {
-					// ... add other private include paths required here ...
+            PrivateIncludePaths.AddRange(
+                new string[] {
+                    // ... add other private include paths required here ...
                     "KlawrCodeGeneratorPlugin/Private"
-				}
-			);
+                }
+            );
 
-			PublicDependencyModuleNames.AddRange(
-				new string[]
-				{
-					"Core",
-					"CoreUObject",
+            PublicDependencyModuleNames.AddRange(
+                new string[]
+                {
+                    "Core",
+                    "CoreUObject",
                     // ... add other public dependencies that you statically link with here ...
-				}
-			);
+                }
+            );
 
-			PrivateDependencyModuleNames.AddRange(
-				new string[]
-				{
-    				"Projects"
-					// ... add private dependencies that you statically link with here ...
-				}
-			);
+            PrivateDependencyModuleNames.AddRange(
+                new string[]
+                {
+                    "Projects"
+                    // ... add private dependencies that you statically link with here ...
+                }
+            );
 
-			DynamicallyLoadedModuleNames.AddRange(
-				new string[]
-				{
-					// ... add any modules that your module loads dynamically here ...
-				}
-			);
+            DynamicallyLoadedModuleNames.AddRange(
+                new string[]
+                {
+                    // ... add any modules that your module loads dynamically here ...
+                }
+            );
             
 
-			var pluginPath = Path.Combine("..", @"Plugins\Klawr\Klawr\Source\ThirdParty\Klawr", Target.Platform.ToString(), "Release", "Klawr.ClrHost.Native-x64-Release.dll");
+            var pluginPath = Path.Combine("..", @"Plugins\Klawr\Klawr\Source\ThirdParty\Klawr", Target.Platform.ToString(), "Release", "Klawr.ClrHost.Native-x64-Release.dll");
             if(File.Exists(pluginPath)) {
                 Definitions.Add("KLAWRGEN_PATH=" + ModuleDirectory);
                 Definitions.Add("WITH_KLAWR=1");
-        		Log.TraceInformation("Klawr enabled. Module dir: " + ModuleDirectory);
+                Log.TraceInformation("Klawr enabled. Module dir: " + ModuleDirectory);
             } else{
-        		Log.TraceInformation("Klawr source directory not found.");                
+                Log.TraceInformation("Klawr source directory not found.");                
             }
-    		Definitions.Add("HACK_HEADER_GENERATOR=1");
+            Definitions.Add("HACK_HEADER_GENERATOR=1");
         }
-	}
+    }
 }
